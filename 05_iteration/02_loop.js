15afe7ae25b6a5aeb21b2b4664c0c4bfad2947c2
KANISHK RAJ <kanishkraj601@gmail.com>
2024-10-02 08:34:28 +0000

    05.02

---
+++ 05_iteration/02_loop.js
@@ -0,0 +1,34 @@
+//  While Loop
+
+let index = 0
+while ( index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+
+
+let myArray = ['flash', 'batman', 'superman']
+
+let arr = 0;
+while (arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr ++;
+}
+
+// Do While loop
+
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`); // score is 1 to 10
+    score++
+} while (score <= 10 );
+
+let point = 11 
+
+do {
+    console.log(`point is ${point}`); // point is 11
+    point++
+} while (point <= 10 );
